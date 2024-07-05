4f82bf8659d094dec8d030566b3294c8dee70a82
zaryabzubair <zaryabzubair.m@gmail.com>
2024-07-06 01:57:18 +0500

    update commit

--- build_files.sh
+++ build_files.sh
@@ -8,7 +8,7 @@ echo "installing dependencies"
 pip install -r requirements.txt
 
 echo "static files"
-python3.9 manage.py collectstatic --noinput
+python3.9 manage.py collectstatic --noinput --clear
 
 echo "Migration"
 python3.9 manage.py makemigrations
