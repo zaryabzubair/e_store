7e1eca7de0e96dca6b5369723515cd17a9f3bc21
zaryabzubair <zaryabzubair.m@gmail.com>
2024-07-06 23:04:57 +0500

    update model

--- store/models.py
+++ store/models.py
@@ -9,7 +9,7 @@ class Product(models.Model):
     available = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    image = models.ImageField(upload_to='products/', blank=True, null=True)
+    image = models.ImageField(upload_to='media/', blank=True, null=True)
 
     def __str__(self):
         return self.name
